724697de452d81db7a58b4365d1fbae0a4b7e72d
Teodor Rožej <teodor.rozej@gmail.com>
2026-04-21 14:14:04 +0200

    Fixed saving the decompiled code to not include 'id', since it is unused

--- create_file.py
+++ create_file.py
@@ -4,4 +4,4 @@ def save_chunks_to_file(chunks, output_file="packets.txt"):
     with open(output_file, "w", encoding="utf-8") as f:
         for chunk in chunks:
             data_str = ",".join(map(str, chunk["data"].flatten()))
-            f.write(f"{chunk['id']};{chunk['timestamp']};{data_str}\n")
+            f.write(f"{chunk['timestamp']};{data_str}\n")
