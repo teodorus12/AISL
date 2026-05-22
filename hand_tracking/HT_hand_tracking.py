6f5fb7f263a7db47fcfb765f40038ac17f80e544
Teodor Rožej <teodor.rozej@gmail.com>
2026-05-22 04:10:56 +0200

    FINALLY working hand_tracking, didn't have to reinvent the wheel!!!

---
+++ hand_tracking/HT_hand_tracking.py
@@ -0,0 +1,43 @@
+from HT_camera_handler import CameraHandler
+from HT_handler import HandTracker
+from HT_landmark_drawer import LandMarkDrawer
+from HT_frame_handler import FrameProcessor
+
+from HT_window import MainWindow
+
+
+class HandTrackingApp:
+    def __init__(self):
+        self.camera_handler = CameraHandler()
+        self.hand_tracker = HandTracker()
+        self.landmark_drawer = LandMarkDrawer()
+
+        self.frame_processor = FrameProcessor(self.hand_tracker, self.landmark_drawer)
+        self.window = MainWindow()
+        self.running = False
+
+    def start(self):
+        self.camera_handler.open_camera()
+        self.running = True
+
+        self.window.set_close_callback(self.stop)
+        self.update_loop()
+        self.window.start()
+
+    def update_loop(self):
+        if not self.running:
+            return
+
+        succ, frame = self.camera_handler.read_frame()
+
+        if succ:
+            processed_frame = self.frame_processor.process(frame)
+            self.window.update_video(processed_frame)
+        self.window.after(10, self.update_loop)
+
+    def stop(self):
+        self.running = False
+
+        self.camera_handler.release_camera()
+        self.hand_tracker.close()
+        self.window.destroy()
---
+++ hand_tracking/HT_main.py
@@ -0,0 +1,8 @@
+from HT_hand_tracking import HandTrackingApp
+
+def main():
+    app = HandTrackingApp()
+    app.start()
+    
+if __name__ == "__main__":
+    main()
